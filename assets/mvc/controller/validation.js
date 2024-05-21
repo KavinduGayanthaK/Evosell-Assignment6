3a33fe5553633d4ab9796708c7e36d1623f30268
Kavindu <gayanthakavindu3@gmail.com>
2024-05-21 11:41:09 +0530

    validation customer fields

--- assets/mvc/controller/validation.js
+++ assets/mvc/controller/validation.js
@@ -23,7 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     validateItemPrice(input);
                 } else if (input.id === "ItemQty") {
                     validateItemQty(input);
-                } else {
+                }else if (input.id === 'customerId') {
+                    validateCustomerId(input);
+                } else if (input.id === 'customerName') {
+                    validateCustomerName(input);
+                } else if (input.id === "customerAddress") {
+                    validateCustomerAddress(input);
+                } else if (input.id === "customerEmail") {
+                    validateCustomerEmail(input);
+                } else if (input.id === "customerNic") {
+                    validateCustomerNic(input);
+                }
+                else {
                     if (!input.checkValidity()) {
                         input.classList.add('is-invalid');
                     } else {
@@ -121,6 +132,121 @@ function validateItemQty(input) {
     } else {
         itemQtyLabel.text(' Item Qty is a required field.Only numbers.');
         itemQtyLabel.css({display: 'block'});
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        input.style.border = '1px solid red';
+        return false;
+    }
+}
+
+function validateCustomerId(input) {
+    let id = input.value;
+    const customerID = $('#customer-id-label');
+
+    if (id.startsWith('C00-')) {
+        const numericPart = id.substring(4);
+
+        if (/^\d{3,}$/.test(numericPart)) {
+            customerID.css({display: 'none'});
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+            $('#customerId').css({border: '1px solid green'});
+            return true;
+        } else {
+            customerID.text('Customer ID must be a minimum 3 digit value after C00- format.');
+            customerID.css({display: 'block'});
+            input.classList.add('is-invalid');
+            input.classList.remove('is-valid');
+            $('#customerId').css({border: '1px solid red'});
+            return false;
+        }
+    } else {
+        customerID.text('Customer Id must start with "C00-');
+        customerID.css({display: 'block'});
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        $('#customerId').css({border: '1px solid red'});
+        return false;
+    }
+}
+
+function validateCustomerName(input) {
+    let customerName = input.value;
+    const customerLabel = $('#customer-name-label');
+
+    let trimName = customerName.trim();
+    if (trimName.length >= 5 && trimName.length <= 20 && /^[a-zA-Z ]+$/.test(trimName)) {
+        customerLabel.css({display: 'none'});
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        input.style.border = '1px solid green';
+        return true;
+    } else {
+        customerLabel.text(' Customer name is required field:Minimum 5,max 20,space allowed.');
+        customerLabel.css({display: 'block'});
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        input.style.border = '1px solid red';
+        return false;
+    }
+}
+
+function validateCustomerAddress(input) {
+    let customerAddress = input.value;
+    const customerLabel = $('#customer-address-label');
+
+    let trimName = customerAddress.trim();
+    if (trimName.length >= 5 && trimName.length <= 60 && /^No\d+,?\s[A-Za-z]+,?\s[A-Za-z]+$/.test(trimName)) {
+        customerLabel.css({display: 'none'});
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        input.style.border = '1px solid green';
+        return true;
+    } else {
+        customerLabel.text('Customer address is required field:Minimum 5,max 60,space allowed.');
+        customerLabel.css({display: 'block'});
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        input.style.border = '1px solid red';
+        return false;
+    }
+}
+
+function validateCustomerEmail(input) {
+    let customerEmail = input.value;
+    const customerLabel = $('#customer-email-label');
+
+    let trimName = customerEmail.trim();
+    if (trimName.length >= 5 && trimName.length <= 60 && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(trimName)) {
+        customerLabel.css({display: 'none'});
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        input.style.border = '1px solid green';
+        return true;
+    } else {
+        customerLabel.text('Please enter a valid email address.');
+        customerLabel.css({display: 'block'});
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+        input.style.border = '1px solid red';
+        return false;
+    }
+}
+
+function validateCustomerNic(input) {
+    let customerNic = input.value;
+    const customerLabel = $('#customer-nic-label');
+
+    const nicPattern = /^\d{9}[VX]|\d{12}$/;
+    if (nicPattern.test(customerNic)) {
+        customerLabel.css({display: 'none'});
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+        input.style.border = '1px solid green';
+        return true;
+    } else {
+        customerLabel.text('NIC must be in the format "123456789V", "123456789X", or "200043502345".');
+        customerLabel.css({display: 'block'});
         input.classList.add('is-invalid');
         input.classList.remove('is-valid');
         input.style.border = '1px solid red';
