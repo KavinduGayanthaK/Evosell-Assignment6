d762b900b50e9d80d9e6d16cd3d3f7c0e7bf71be
Kavindu <gayanthakavindu3@gmail.com>
2024-05-26 10:41:24 +0530

    create user model class

---
+++ assets/mvc/model/UserModel.js
@@ -0,0 +1,23 @@
+export default class UserModel{
+    constructor(username,password){
+        this._username = username;
+        this._password = password;
+    }
+
+
+    get username() {
+        return this._username;
+    }
+
+    set username(value) {
+        this._username = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+}
